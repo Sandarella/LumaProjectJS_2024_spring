6387814326916f0a31b6e54d9db273377a32d269
Anton Kolupaiev <antnikol@gmail.com>
2024-12-12 15:02:44 +0200

    upd classes creation

--- tests/test_page_objects/menBottomsBreadcrumbs.spec.js
+++ tests/test_page_objects/menBottomsBreadcrumbs.spec.js
@@ -11,26 +11,32 @@ import {
 import MenPage from "../../page_objects/menPage";
 import MenBottomsPage from "../../page_objects/menBottomsPage";
 
-test('Men have breadcrumb navigation on the “Men/Bottoms” page and back', async ({ page }) => {
-    const homePage = new HomePage(page);
-    const menBottomsPage = new MenBottomsPage(page);
-    const menPage = new MenPage(page);
+let homePage, menBottomsPage, menPage
 
-    await homePage.open();
-    await homePage.hoverMenLink();
-    await homePage.clickMenBottomsLink();
+test.describe('logo.spec', () => {
+    test.beforeEach(async ({page}) => {
+        homePage = new HomePage(page);
+        menBottomsPage = new MenBottomsPage(page);
+        menPage = new MenPage(page);
+        await homePage.open();
+    })
 
-    await expect(page).toHaveURL(MEN_BOTTOMS_PAGE_END_POINT);
-    await expect(menBottomsPage.locators.getMenBottomsBreadcrumbs()).toBeVisible();
-    await expect(menBottomsPage.locators.getBreadcrumbsMenuHome()).toHaveText(MEN_BOTTOMS_BREADCRUMBS_MENU_HOME_TEXT);
-    await expect(menBottomsPage.locators.getBreadcrumbsMenuMen()).toHaveText(MEN_BOTTOMS_BREADCRUMBS_MENU_MEN_TEXT);
-    await expect(menBottomsPage.locators.getBreadcrumbsMenuBottoms()).toHaveText(MEN_BOTTOMS_BREADCRUMBS_MENU_BOTTOMS_TEXT);
-
-    await menBottomsPage.clickBreadcrumbsMenuMen();
-
-    await expect(page).toHaveURL(MEN_PAGE_END_POINT);
-
-    await menPage.clickBeadcrumbsMenuHome();
-
-    await expect(page).toHaveURL(BASE_URL);
+    test('Men have breadcrumb navigation on the “Men/Bottoms” page and back', async ({ page }) => {
+        await homePage.hoverMenLink();
+        await homePage.clickMenBottomsLink();
+    
+        await expect(page).toHaveURL(MEN_BOTTOMS_PAGE_END_POINT);
+        await expect(menBottomsPage.locators.getMenBottomsBreadcrumbs()).toBeVisible();
+        await expect(menBottomsPage.locators.getBreadcrumbsMenuHome()).toHaveText(MEN_BOTTOMS_BREADCRUMBS_MENU_HOME_TEXT);
+        await expect(menBottomsPage.locators.getBreadcrumbsMenuMen()).toHaveText(MEN_BOTTOMS_BREADCRUMBS_MENU_MEN_TEXT);
+        await expect(menBottomsPage.locators.getBreadcrumbsMenuBottoms()).toHaveText(MEN_BOTTOMS_BREADCRUMBS_MENU_BOTTOMS_TEXT);
+    
+        await menBottomsPage.clickBreadcrumbsMenuMen();
+    
+        await expect(page).toHaveURL(MEN_PAGE_END_POINT);
+    
+        await menPage.clickBeadcrumbsMenuHome();
+    
+        await expect(page).toHaveURL(BASE_URL);
+    })
 })
