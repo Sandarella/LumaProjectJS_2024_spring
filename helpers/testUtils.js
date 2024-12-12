e864eb27fe8c3ea14e832c92e7b7f53989faf356
Anton Kolupaiev <antnikol@gmail.com>
2024-12-12 18:59:47 +0200

    upd skiped tests + URL_regexp function

--- helpers/testUtils.js
+++ helpers/testUtils.js
@@ -2,10 +2,9 @@ export const getRandomNumber = (max) =>
     Math.floor(Math.random() * max);
 
 export const urlToRegexPattern = (url) => {
-    const escapedUrl = url.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    const regexPattern = escapedUrl.replace(/\*/g, '.*');
-
-    return `${regexPattern}`;
+    const escapedUrl = url.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
+    const regexPattern = escapedUrl.replace(/\\\*/g, '.*');
+    return `^${regexPattern}$`; 
 };
 
 export function generateRandomEmail() {
